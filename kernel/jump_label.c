04eb3f66fe4ed0921798d1dcb69dda4d37071f3a
freak07 <michalechner92@googlemail.com>
2019-02-20 10:22:43 +0100

    techpack: use power efficient workingqueues
    platform: ipa: use power efficient workingqueues
    
    kernel: use power efficient workingqueues

--- kernel/jump_label.c
+++ kernel/jump_label.c
@@ -238,7 +238,11 @@ void static_key_slow_dec_cpuslocked(struct static_key *key)
 void static_key_slow_dec_deferred(struct static_key_deferred *key)
 {
 	STATIC_KEY_CHECK_USE();
-	__static_key_slow_dec(&key->key, key->timeout, &key->work);
+
+	if (static_key_slow_try_dec(key))
+		return;
+
+	queue_delayed_work(system_power_efficient_wq, work, timeout);
 }
 EXPORT_SYMBOL_GPL(static_key_slow_dec_deferred);
 
