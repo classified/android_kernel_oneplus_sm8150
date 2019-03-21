e2db32d1067ca7ce00b0b05e3868882f5db0a41d
Wilson Sung <wilsonsung@google.com>
2019-03-21 10:21:24 +0800

    Revert "mm: reduce the time spend by killed tasks in alloc path"
    This reverts commit c31eadb2f77c479b41edd6900e22cc28005f5974.
    
    Bug: 128591321
    Bug: 128639903
    Bug: 127924739
    Change-Id: I4c022958a52cf6e82130a1dc87bc73210ad53245
    Signed-off-by: Wilson Sung <wilsonsung@google.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -4411,10 +4411,6 @@ retry:
 	if (current->flags & PF_MEMALLOC)
 		goto nopage;
 
-	if (fatal_signal_pending(current) && !(gfp_mask & __GFP_NOFAIL) &&
-			(gfp_mask & __GFP_FS))
-		goto nopage;
-
 	/* Try direct reclaim and then allocating */
 	if (!woke_kshrinkd) {
 		/*
