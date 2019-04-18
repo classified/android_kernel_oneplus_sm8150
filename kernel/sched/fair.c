171b79f9e4310de336912b946c085d85ffe6bd20
Pavankumar Kondeti <pkondeti@codeaurora.org>
2019-04-18 14:24:30 +0530

    sched/fair: Optimize the tick path active migration
    When a task is upmigrating via tickpath, the lower capacity CPU
    that is running the task will wake up the migration task to
    carry the migration to the other higher capacity CPU. The migration
    task dequeue the task from lower capacity CPU and enqueue it on
    the higher capacity CPU. A rescheduler IPI is sent now to the higher
    capacity CPU. If the higher capacity CPU was in deep sleep state, it
    results in more waiting time for the task to be upmigrated. This can
    be optimized by waking up the higher capacity CPU along with waking
    the migration task on the lower capacity CPU. Since we reserve the
    higher capacity CPU, the is_reserved() API can be used to prevent
    the CPU entering idle again.
    
    Change-Id: I7bda9a905a66a9326c1dc74e50fa94eb58e6b705
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    [clingutla@codeaurora.org: Resolved minor merge conflicts]
    Signed-off-by: Lingutla Chandrasekhar <clingutla@codeaurora.org>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>
    (cherry picked from commit 6443f55a3bf9d2dfb72a497d37aa46f231640ab4)
    Signed-off-by: Rajan Palaniya <razorpaul@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -13440,11 +13440,12 @@ bool ux_task_misfit(struct task_struct *p, int cpu)
 static DEFINE_RAW_SPINLOCK(migration_lock);
 void check_for_migration(struct rq *rq, struct task_struct *p)
 {
-	int active_balance;
+	int active_balance, ret;
 	int new_cpu = -1;
 	int cpu = smp_processor_id();
 	int prev_cpu = task_cpu(p);
 	struct sched_domain *sd = NULL;
+	int ret;
 
 #ifdef OPLUS_FEATURE_SCHED_ASSIST
 	if (rq->misfit_task_load || (sched_assist_scene(SA_SLIDE) &&
@@ -13470,9 +13471,13 @@ void check_for_migration(struct rq *rq, struct task_struct *p)
 			if (active_balance) {
 				mark_reserved(new_cpu);
 				raw_spin_unlock(&migration_lock);
-				stop_one_cpu_nowait(prev_cpu,
+				ret = stop_one_cpu_nowait(prev_cpu,
 					active_load_balance_cpu_stop, rq,
 					&rq->active_balance_work);
+				if (!ret)
+					clear_reserved(new_cpu);
+				else
+					wake_up_if_idle(new_cpu);
 				return;
 			}
 		} else {
