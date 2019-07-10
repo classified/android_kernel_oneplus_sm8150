b659b76eee133f33576c9d099c02ead1892b16ee
Yaroslav Furman <yaro330@gmail.com>
2019-07-11 02:36:23 +0300

    kernel: bpf: move syscall allocations to stack
    These are really small, freed in the same function, very frequent.
    Allocating them on stack will improve performance.
    
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -500,6 +500,7 @@ static int map_lookup_elem(union bpf_attr *attr)
 	int ufd = attr->map_fd;
 	struct bpf_map *map;
 	void *key, *value, *ptr;
+	char value_onstack[64];
 	u32 value_size;
 	struct fd f;
 	int err;
@@ -533,9 +534,15 @@ static int map_lookup_elem(union bpf_attr *attr)
 		value_size = map->value_size;
 
 	err = -ENOMEM;
-	value = kmalloc(value_size, GFP_USER | __GFP_NOWARN);
-	if (!value)
-		goto free_key;
+
+	if (value_size < ARRAY_SIZE(value_onstack)) {
+		value = value_onstack;
+		memset(value, 0, sizeof(value));
+	} else {
+		value = kmalloc(value_size, GFP_USER | __GFP_NOWARN);
+		if (!value)
+			goto free_key;
+	}
 
 	if (map->map_type == BPF_MAP_TYPE_PERCPU_HASH ||
 	    map->map_type == BPF_MAP_TYPE_LRU_PERCPU_HASH) {
@@ -571,7 +578,8 @@ static int map_lookup_elem(union bpf_attr *attr)
 	err = 0;
 
 free_value:
-	kfree(value);
+	if (value != value_onstack)
+		kfree(value);
 free_key:
 	kfree(key);
 err_put:
@@ -738,6 +746,7 @@ static int map_get_next_key(union bpf_attr *attr)
 	int ufd = attr->map_fd;
 	struct bpf_map *map;
 	void *key, *next_key;
+	char next_key_onstack[64];
 	struct fd f;
 	int err;
 
@@ -765,9 +774,14 @@ static int map_get_next_key(union bpf_attr *attr)
 	}
 
 	err = -ENOMEM;
-	next_key = kmalloc(map->key_size, GFP_USER);
-	if (!next_key)
-		goto free_key;
+	if (map->key_size < ARRAY_SIZE(next_key_onstack)) {
+		next_key = next_key_onstack;
+		memset(next_key, 0, sizeof(next_key));
+	} else {
+		next_key = kmalloc(map->key_size, GFP_USER | __GFP_NOWARN);
+		if (!next_key)
+			goto free_key;
+	}
 
 	rcu_read_lock();
 	err = map->ops->map_get_next_key(map, key, next_key);
@@ -783,7 +797,8 @@ static int map_get_next_key(union bpf_attr *attr)
 	err = 0;
 
 free_next_key:
-	kfree(next_key);
+	if (next_key != next_key_onstack)
+		kfree(next_key);
 free_key:
 	kfree(key);
 err_put:
