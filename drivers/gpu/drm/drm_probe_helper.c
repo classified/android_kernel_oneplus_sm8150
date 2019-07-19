9937160fd59165fb52e1999889249d7a232a0315
freak07 <michalechner92@googlemail.com>
2019-07-19 10:46:24 +0200

    gpu: drm: Use power efficient workingqueues
    Change-Id: I9c5784dad2b5466f3cff78f0551c2f7e89d0364a

--- drivers/gpu/drm/drm_probe_helper.c
+++ drivers/gpu/drm/drm_probe_helper.c
@@ -258,7 +258,8 @@ void drm_kms_helper_poll_enable(struct drm_device *dev)
 	}
 
 	if (poll)
-		schedule_delayed_work(&dev->mode_config.output_poll_work, delay);
+		queue_delayed_work(system_power_efficient_wq, 
+							&dev->mode_config.output_poll_work, delay);
 }
 EXPORT_SYMBOL(drm_kms_helper_poll_enable);
 
@@ -466,7 +467,7 @@ retry:
 		 */
 		dev->mode_config.delayed_event = true;
 		if (dev->mode_config.poll_enabled)
-			mod_delayed_work(system_wq,
+			mod_delayed_work(system_power_efficient_wq,
 					 &dev->mode_config.output_poll_work,
 					 0);
 	}
@@ -675,7 +676,8 @@ out:
 		drm_kms_helper_hotplug_event(dev);
 
 	if (repoll)
-		schedule_delayed_work(delayed_work, DRM_OUTPUT_POLL_PERIOD);
+		queue_delayed_work(system_power_efficient_wq, delayed_work, 
+							DRM_OUTPUT_POLL_PERIOD);
 }
 
 /**
