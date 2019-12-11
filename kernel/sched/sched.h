5f5ca92fd115bf8f47a60296886cdc63464dab24
AngeloGioacchino Del Regno <kholk11@gmail.com>
2019-12-11 18:43:05 +0100

    sched: Add mark_reserved no-op for !CONFIG_SCHED_WALT
    When WALT is disabled, this function must be present anyway:
    add a noop.
    
    Change-Id: I5ac8a4578df79cf3916dc4ae37eb1d3dae00e1b2
    Signed-off-by: atndko <z1281552865@gmail.com>
    Signed-off-by: Zlatan Radovanovic <zlatan.radovanovic@fet.ba>
    Signed-off-by: Rajan Palaniya <razorpaul@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -3137,6 +3137,11 @@ static inline int is_reserved(int cpu)
 	return 0;
 }
 
+static inline int mark_reserved(int cpu)
+{
+	return 0;
+}
+
 extern void sched_boost_parse_dt(void);
 
 static inline void clear_ed_task(struct task_struct *p, struct rq *rq) { }
