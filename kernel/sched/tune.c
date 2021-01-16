59b5414782f6fb47b86954df1ba0847e93c71bf8
Yaroslav Furman <yaro330@gmail.com>
2021-01-16 13:54:44 +0300

    sched/tune: only force UX tasks to big cores
    If we force top-app's boost value to be >0 we get excellent
    jitter values due to top-app being rendered on big cores.
    
    However along with the actual top app a load of crap ends up on
    big cluster too. We can avoid this by checking if adj_score is 0,
    which means that this app is actually the one that's currently on
    the screen (or it's one of its child processes).
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    [alk3pInjection: Squash F1xy-kernels/SURYA_kernel@68ebacb]
    Signed-off-by: Rajan Palaniya <razorpaul@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -489,6 +489,31 @@ int schedtune_cpu_boost(int cpu)
 #ifdef OPLUS_FEATURE_UIFIRST
 extern bool test_task_ux(struct task_struct *task);
 #endif /* OPLUS_FEATURE_UIFIRST */
+
+static inline int schedtune_adj_ta(struct task_struct *p)
+{
+	struct schedtune *st;
+	char name_buf[NAME_MAX + 1];
+	int adj = p->signal->oom_score_adj;
+
+	/* We only care about adj == 0 */
+	if (adj != 0)
+		return 0;
+
+	/* Don't touch kthreads */
+	if (p->flags & PF_KTHREAD)
+		return 0;
+
+	st = task_schedtune(p);
+	cgroup_name(st->css.cgroup, name_buf, sizeof(name_buf));
+	if (!strncmp(name_buf, "top-app", strlen("top-app"))) {
+		pr_debug("top app is %s with adj %i\n", p->comm, adj);
+		return 1;
+	}
+
+	return 0;
+}
+
 int schedtune_task_boost(struct task_struct *p)
 {
 	struct schedtune *st;
@@ -500,7 +525,7 @@ int schedtune_task_boost(struct task_struct *p)
 	/* Get task boost value */
 	rcu_read_lock();
 	st = task_schedtune(p);
-	task_boost = st->boost;
+	task_boost = st->boost * schedtune_adj_ta(p);
 #ifdef OPLUS_FEATURE_UIFIRST
 	if (sysctl_uifirst_enabled && sysctl_launcher_boost_enabled && p->static_ux == 2) {
 		task_boost = 60;
@@ -524,7 +549,7 @@ int schedtune_task_boost_rcu_locked(struct task_struct *p)
 
 	/* Get task boost value */
 	st = task_schedtune(p);
-	task_boost = st->boost;
+	task_boost = st->boost * schedtune_adj_ta(p);
 
 	return task_boost;
 }
