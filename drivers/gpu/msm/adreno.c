92adc8d52f9513a920e90ebf6f6e9120aa92e719
Daniel Jacob Chittoor <djchittoor47@gmail.com>
2021-06-08 15:29:57 +0200

    msm: kgsl: Omit GPU CoreSight code if !CORESIGHT
    Signed-off-by: Daniel Jacob Chittoor <djchittoor47@gmail.com>
    Signed-off-by: Rajan Palaniya <razorpaul@gmail.com>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -1305,8 +1305,10 @@ static int adreno_probe(struct platform_device *pdev)
 
 	kgsl_pwrscale_init(&pdev->dev, CONFIG_QCOM_ADRENO_DEFAULT_GOVERNOR);
 
+	#ifdef CONFIG_CORESIGHT
 	/* Initialize coresight for the target */
 	adreno_coresight_init(adreno_dev);
+	#endif
 
 	/* Get the system cache slice descriptor for GPU */
 	adreno_dev->gpu_llc_slice = adreno_llc_getd(&pdev->dev, "gpu");
@@ -1378,7 +1380,9 @@ static int adreno_remove(struct platform_device *pdev)
 
 	adreno_sysfs_close(adreno_dev);
 
+	#ifdef CONFIG_CORESIGHT
 	adreno_coresight_remove(adreno_dev);
+	#endif
 	adreno_profile_close(adreno_dev);
 
 	/* Release the system cache slice descriptor */
@@ -1999,8 +2003,10 @@ static int _adreno_start(struct adreno_device *adreno_dev)
 	 */
 	adreno_llc_setup(device);
 
+	#ifdef CONFIG_CORESIGHT
 	/* Re-initialize the coresight registers if applicable */
 	adreno_coresight_start(adreno_dev);
+	#endif
 
 	adreno_irqctrl(adreno_dev, 1);
 
@@ -2142,8 +2148,10 @@ static int adreno_stop(struct kgsl_device *device)
 	adreno_llc_deactivate_slice(adreno_dev->gpu_llc_slice);
 	adreno_llc_deactivate_slice(adreno_dev->gpuhtw_llc_slice);
 
+	#ifdef CONFIG_CORESIGHT
 	/* Save active coresight registers if applicable */
 	adreno_coresight_stop(adreno_dev);
+	#endif
 
 	/* Save physical performance counter values before GPU power down*/
 	adreno_perfcounter_save(adreno_dev);
@@ -2994,8 +3002,10 @@ int adreno_soft_reset(struct kgsl_device *device)
 	/* Reinitialize the GPU */
 	gpudev->start(adreno_dev);
 
+	#ifdef CONFIG_CORESIGHT
 	/* Re-initialize the coresight registers if applicable */
 	adreno_coresight_start(adreno_dev);
+	#endif
 
 	/* Enable IRQ */
 	adreno_irqctrl(adreno_dev, 1);
