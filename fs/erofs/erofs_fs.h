e43cbaef65d588b358959cc6dacd4b09a24ad79e
Gao Xiang <hsiangkao@linux.alibaba.com>
2021-08-30 11:44:53 +0800

    erofs: move from drivers/staging/ to fs/
    Since 5.4, erofs has been moved into fs/.
    
    Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    Change-Id: I95dd967a0097629a9d8eaed1dc11e2cd04f47701
    Signed-off-by: Rajan Palaniya <razorpaul@gmail.com>

--- fs/erofs/erofs_fs.h
+++ fs/erofs/erofs_fs.h
@@ -9,7 +9,10 @@
 #ifndef __EROFS_FS_H
 #define __EROFS_FS_H
 
+#define EROFS_SUPER_MAGIC_V1	0xE0F5E1E2
 #define EROFS_SUPER_OFFSET      1024
+
+#define EROFS_FEATURE_COMPAT_SB_CHKSUM          0x00000001
 
 /*
  * Any bits that aren't in EROFS_ALL_FEATURE_INCOMPAT should
@@ -37,7 +40,6 @@ struct erofs_super_block {
 	__u8 uuid[16];          /* 128-bit uuid for volume */
 	__u8 volume_name[16];   /* volume name */
 	__le32 feature_incompat;
-
 	__u8 reserved2[44];
 };
 
@@ -73,6 +75,9 @@ static inline bool erofs_inode_is_data_compressed(unsigned int datamode)
 
 #define EROFS_I_VERSION_BIT             0
 #define EROFS_I_DATALAYOUT_BIT          1
+
+#define EROFS_I_ALL	\
+	((1 << (EROFS_I_DATALAYOUT_BIT + EROFS_I_DATALAYOUT_BITS)) - 1)
 
 /* 32-byte reduced form of an ondisk inode */
 struct erofs_inode_compact {
@@ -320,4 +325,3 @@ static inline void erofs_check_ondisk_layout_definitions(void)
 }
 
 #endif
-
