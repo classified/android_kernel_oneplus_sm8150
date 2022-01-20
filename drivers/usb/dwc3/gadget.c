b20e975a2c68f6c10ef4458ff2ca6ea124eefc45
Arian <arian.kulmer@web.de>
2022-01-20 18:11:49 +0100

    Squashed rollback for upstream fixes
    This reverts the following commits:
    
    3e674f8 "usb: gadget: configfs: Replace strncpy with strlcpy"
    cd86a45 "CHROMIUM: usb: gadget: configfs: Fix KASAN use-after-free"
    7388183 "usb: dwc3: Avoid NULL pointer access"
    7668c04 "usb: dwc3: gadget: Avoid use-after-free of stale requests"
    3aa0379 "usb: dwc3: gadget: Fix runtime PM vote from pull_up handler"
    20e6a28 "coresight: tmc: Read the TMC mode register only if clk is enabled"
    518ad56 "usb: dwc3: gadget: Prevent core from processing stale TRBs"
    9670ea2 "USB: rndis: Protect rndis response queue"
    9277207 "ipv6: remove min MTU check for ipsec tunnels"
    
    Change-Id: Iaf027bc54c1f1c5e2073431d2e49748961a51812
    Signed-off-by: Rajan Palaniya <razorpaul@gmail.com>

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -1193,19 +1193,6 @@ static void __dwc3_prepare_one_trb(struct dwc3_ep *dep, struct dwc3_trb *trb,
 	if (usb_endpoint_xfer_bulk(dep->endpoint.desc) && dep->stream_capable)
 		trb->ctrl |= DWC3_TRB_CTRL_SID_SOFN(stream_id);
 
-	/*
-	 * As per data book 4.2.3.2TRB Control Bit Rules section
-	 *
-	 * The controller autonomously checks the HWO field of a TRB to determine if the
-	 * entire TRB is valid. Therefore, software must ensure that the rest of the TRB
-	 * is valid before setting the HWO field to '1'. In most systems, this means that
-	 * software must update the fourth DWORD of a TRB last.
-	 *
-	 * However there is a possibility of CPU re-ordering here which can cause
-	 * controller to observe the HWO bit set prematurely.
-	 * Add a write memory barrier to prevent CPU re-ordering.
-	 */
-	wmb();
 	trb->ctrl |= DWC3_TRB_CTRL_HWO;
 
 	dwc3_ep_inc_enq(dep);
@@ -1267,6 +1254,7 @@ static struct dwc3_trb *dwc3_ep_prev_trb(struct dwc3_ep *dep, u8 index)
 
 static u32 dwc3_calc_trbs_left(struct dwc3_ep *dep)
 {
+	struct dwc3_trb		*tmp;
 	u8			trbs_left;
 
 	/*
@@ -1275,11 +1263,8 @@ static u32 dwc3_calc_trbs_left(struct dwc3_ep *dep)
 	 * pending to be processed by the driver.
 	 */
 	if (dep->trb_enqueue == dep->trb_dequeue) {
-		/*
-		 * If there is any request remained in the started_list at
-		 * this point, that means there is no TRB available.
-		 */
-		if (!list_empty(&dep->started_list))
+		tmp = dwc3_ep_prev_trb(dep, dep->trb_enqueue);
+		if (tmp->ctrl & DWC3_TRB_CTRL_HWO)
 			return 0;
 
 		return DWC3_TRB_NUM - 1;
@@ -2332,8 +2317,10 @@ static int dwc3_gadget_pullup(struct usb_gadget *g, int is_on)
 
 		ret = wait_for_completion_timeout(&dwc->ep0_in_setup,
 				msecs_to_jiffies(DWC3_PULL_UP_TIMEOUT));
-		if (ret == 0)
-			dev_warn(dwc->dev, "timed out waiting for SETUP phase\n");
+		if (ret == 0) {
+			dev_err(dwc->dev, "timed out waiting for SETUP phase\n");
+			return -ETIMEDOUT;
+		}
 	}
 
 	disable_irq(dwc->irq);
