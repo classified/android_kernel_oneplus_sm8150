3e2a205ac809574458925d596933504496211954
yu.huang <yu.huang@oneplus.com>
2022-04-04 19:47:26 +0530

    Synchronize codes for OnePlus 7 Oxygen OS 11.GM21AA and OnePlus 7Pro Oxygen OS 11.GM21AA and OnePlus 7T Oxygen OS 11.HD01AA and OnePlus 7TPro Oxygen OS 11.HD01AA

--- drivers/media/platform/msm/camera/cam_req_mgr/cam_req_mgr_util.c
+++ drivers/media/platform/msm/camera/cam_req_mgr/cam_req_mgr_util.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2017-2019,2021 The Linux Foundation. All rights reserved.
+/* Copyright (c) 2017-2019, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -189,7 +189,6 @@ int32_t cam_create_device_hdl(struct cam_create_dev_hdl *hdl_data)
 	hdl_tbl->hdl[idx].state = HDL_ACTIVE;
 	hdl_tbl->hdl[idx].priv = hdl_data->priv;
 	hdl_tbl->hdl[idx].ops = hdl_data->ops;
-	hdl_tbl->hdl[idx].dev_id = hdl_data->dev_id;
 	spin_unlock_bh(&hdl_tbl_lock);
 
 	pr_debug("%s: handle = %x", __func__, handle);
