0dd7db50012df5e4b7b34909d6969a46877bdde5
alk3pInjection <webmaster@raspii.tech>
2022-04-18 20:01:27 +0530

    drm: Implement panel status check
    * to ensure dimlayer has been killed when the panel is off.
    
    Co-authored-by: Art_Chen <Chenxy0201@qq.com>
    Change-Id: I87c861e945eb8728dfd656b20de7ba4fe50b0f09

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -4643,6 +4643,7 @@ error:
 	return rc;
 }
 
+extern int oneplus_panel_status;
 int dsi_panel_set_lp1(struct dsi_panel *panel)
 {
 	int rc = 0;
@@ -4674,6 +4675,7 @@ int dsi_panel_set_lp1(struct dsi_panel *panel)
 		       panel->name, rc);
 
 	panel->need_power_on_backlight = true;
+	oneplus_panel_status = 3; // DISPLAY_POWER_DOZE
 exit:
 	mutex_unlock(&panel->panel_lock);
 	return rc;
@@ -4696,6 +4698,7 @@ int dsi_panel_set_lp2(struct dsi_panel *panel)
 	if (rc)
 		pr_err("[%s] failed to send DSI_CMD_SET_LP2 cmd, rc=%d\n",
 		       panel->name, rc);
+	oneplus_panel_status = 4; // DISPLAY_POWER_DOZE_SUSPEND
 exit:
 	mutex_unlock(&panel->panel_lock);
 	return rc;
@@ -4726,6 +4729,7 @@ int dsi_panel_set_nolp(struct dsi_panel *panel)
 	if (rc)
 		pr_err("[%s] failed to send DSI_CMD_SET_NOLP cmd, rc=%d\n",
 		       panel->name, rc);
+	oneplus_panel_status = 2; // DISPLAY_POWER_ON
 exit:
 	mutex_unlock(&panel->panel_lock);
 	return rc;
@@ -5121,6 +5125,7 @@ int dsi_panel_enable(struct dsi_panel *panel)
 	}
 
 	panel->panel_initialized = true;
+	oneplus_panel_status = 2; // DISPLAY_POWER_ON
 	pr_err("dsi_panel_enable aod_mode =%d\n", panel->aod_mode);
 
 	oneplus_dimlayer_hbm_enable = backup_dimlayer_hbm;
@@ -5235,6 +5240,7 @@ int dsi_panel_disable(struct dsi_panel *panel)
 	}
 	panel->panel_initialized = false;
 	panel->power_mode = SDE_MODE_DPMS_OFF;
+	oneplus_panel_status = 0; // DISPLAY_POWER_OFF
 
 	mutex_unlock(&panel->panel_lock);
 	return rc;
