d1b07ff6839409046f7bb115aca19da56e0e5da4
Nauval Rizky <enuma.alrizky@gmail.com>
2022-06-17 12:35:46 +0700

    extcon: Allocate blocking_notifier_head after registering the device
    Commit 6e721f3ad0535b24f19a62420f4da95212cf069c
    ("extcon: Modify extcon device to be created after driver data is set")
    moved device_register right before adding extcon_dev_list.
    
    This is fine on upstream, however since we have blocking notifier and
    it uses managed kzalloc (which has to be used when the dev is already
    registered successfully), we have to move it as well.
    
    Fixes: a4febd54c189fc2cb63205461bdd58e909e0fb66
    ("extcon: Add usage of blocking notifier chain")
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>
    Change-Id: I93da9ffc39e36545d025aaf91a8433a8d79c376f

--- drivers/extcon/extcon.c
+++ drivers/extcon/extcon.c
@@ -1314,13 +1314,6 @@ int extcon_dev_register(struct extcon_dev *edev)
 		goto err_dev;
 	}
 
-	edev->bnh = devm_kzalloc(&edev->dev,
-			sizeof(*edev->bnh) * edev->max_supported, GFP_KERNEL);
-	if (!edev->bnh) {
-		ret = -ENOMEM;
-		goto err_dev;
-	}
-
 	for (index = 0; index < edev->max_supported; index++)
 		RAW_INIT_NOTIFIER_HEAD(&edev->nh[index]);
 
@@ -1328,6 +1321,13 @@ int extcon_dev_register(struct extcon_dev *edev)
 
 	dev_set_drvdata(&edev->dev, edev);
 	edev->state = 0;
+
+	edev->bnh = devm_kzalloc(&edev->dev,
+			sizeof(*edev->bnh) * edev->max_supported, GFP_KERNEL);
+	if (!edev->bnh) {
+		ret = -ENOMEM;
+		goto err_dev;
+	}
 
 	mutex_lock(&extcon_dev_list_lock);
 	list_add(&edev->entry, &extcon_dev_list);
